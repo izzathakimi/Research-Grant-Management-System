d7407bf080ba00601e1463f2dd75347740850ca7
Izzat <izzathakimi0204@gmail.com>
2025-01-12 20:24:57 +0800

    Route Resource

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
+use App\Http\Controllers\AcademicianController;
+use App\Http\Controllers\GrantProjectController;
+use App\Http\Controllers\MilestoneController;
+use App\Http\Controllers\StaffController;
+use App\Http\Controllers\ProjectMemberController;
+use App\Http\Controllers\Auth\LoginController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('/academicians',\App\Http\Controllers\AcademicianController::class);
+Route::resource('/grant-projects', GrantProjectController::class);
+Route::resource('/milestones', MilestoneController::class);
+Route::resource('/project-members', ProjectMemberController::class);
