2228bbc174c86f681a7131cf817e44b70276453a
Izzat <izzathakimi0204@gmail.com>
2025-01-15 13:39:06 +0800

    Add Role Project Leader

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\Gate;
+use App\Models\User;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -22,5 +24,11 @@ class AppServiceProvider extends ServiceProvider
     {
         //
             Schema::defaultStringLength(191);
+
+        Gate::define('list-feedback', function (User $user) {
+            return in_array($user->role, ['admin', 'staff']); // Only admin and staff can access
+            // OR
+            return $user->role === 'admin'; // Only admin can access
+        });
     }
 }
